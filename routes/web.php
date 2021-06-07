f1b301b7b696efc4e9349a7dff4037e0587135c5
enrico bianco <biancoenrico71@gmail.com>
2021-06-07 20:03:08 +0200

    finito

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/home', function () {
     return view('welcome');
-});
+})->name('laravel');
+
